27d23e49c3918535828c1c780fa21b9a43efc630
Techchauhan <rishabkumarchauhan@gmail.com>
2023-11-03 11:45:25 +0530

    remove unused import files

--- lib/crud/Get Patient/GetPatient.dart
+++ lib/crud/Get Patient/GetPatient.dart
@@ -4,10 +4,6 @@ import 'package:docscriberlite/crud/Get%20Patient/userDetailDialog.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
-import 'package:printing/printing.dart';
-import 'package:flutter/services.dart';
 
 class GetPatient extends StatefulWidget {
   const GetPatient({Key? key}) : super(key: key);
